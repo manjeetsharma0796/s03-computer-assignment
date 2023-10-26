05c0d325e47040dd88017f129819383261f0f838
manjeetsharma0796 <manjeetsharma0796@gmail.com>
2023-10-27 02:46:07 +0530

    add: program ascii value of a character

---
+++ A01P09.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char letter;
+
+    cout<<"Enter letter: ";
+    cin>>letter;
+
+    cout<<"ASCII VALUE: "<<int(letter);
+
+    return 0;
+}
